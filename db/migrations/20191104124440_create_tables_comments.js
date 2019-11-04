2ac16a60a8a76117405a3aacf693df0c924efde0
waivey <waivey@gmail.com>
2019-11-04 12:54:22 +0000

    refactored comments migration set up

--- db/migrations/20191104124440_create_tables_comments.js
+++ db/migrations/20191104124440_create_tables_comments.js
@@ -4,6 +4,9 @@ exports.up = function(knex) {
     commentsTable.increments("comment_id").primary();
     commentsTable.string("author").references("users.username");
     commentsTable.integer("article_id").references("articles.article_id");
+    commentsTable.integer("votes").defaultTo(0);
+    commentsTable.timestamp("create_at").defaultTo(knex.fn.now());
+    commentsTable.text("body").notNullable;
   });
 };
 
